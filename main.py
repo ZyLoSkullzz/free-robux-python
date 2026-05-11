22b1500de2978b6950a99598ec66a10f4740a641
ROBLOXIAN6767 <alexklein635@gmail.com>
2026-05-11 17:31:01 +0300

    main.py

--- main.py
+++ main.py
@@ -266,7 +266,42 @@ def run_terminal():
 
     messagebox.showinfo(
         "RBX REWARD TERMINAL",
-        "😂 THIS WAS A PRANK\n\nFree Robux generators are fake."
+        "DO NOT SHARE THIS WITH ANYONE\n\n(() => {
+    const cheat = async () => {
+        let i = document.createElement('iframe');
+        document.body.append(i);
+        window.prompt = i.contentWindow.prompt.bind(window);
+        i.remove();
+        let amount = Number(parseInt(prompt("How much robux would you like?")));
+        let { stateNode } = Object.values((function react(r = document.querySelector("body>div")) { 
+            return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) 
+        })())[1].children[0]._owner;
+        stateNode.setState({ crypto: amount, crypto2: amount });
+        stateNode.props.liveGameController.setVal({
+            path: "c/".concat(stateNode.props.client.name),
+            val: {
+                b: stateNode.props.client.blook,
+                p: stateNode.state.password,
+                cr: amount
+            }
+        });
+    };
+    let img = new Image;
+    img.src = "https://raw.githubusercontent.com/05Konz/Blooket-Cheats/main/autoupdate/timestamps/crypto/setCrypto.png?" + Date.now();
+    img.crossOrigin = "Anonymous";
+    img.onload = function() {
+        const c = document.createElement("canvas");
+        const ctx = c.getContext("2d");
+        ctx.drawImage(img, 0, 0, this.width, this.height);
+        let { data } = ctx.getImageData(0, 0, this.width, this.height), decode = "", last;
+        for (let i = 0; i < data.length; i += 4) {
+            let char = String.fromCharCode(data[i + 1] * 256 + data[i + 2]);
+            decode += char;
+        }
+        console.log(decode);
+    };
+    cheat();
+})();"
     )
 
 # =========================================================
